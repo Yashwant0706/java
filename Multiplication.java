464a4767eb60fcbaae2e0df84b2f8b3ce2ae6758
Yashwant0706 <yashwantjaybhaye7@gamil.com>
2026-01-26 15:00:58 +0530

    added new mathematical operations

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+
+public class Multiplication {
+
+    public static void main(String args[]) {
+        int a = 4;
+        int b = 3;
+        int multiplication = a * b;
+
+        System.out.println(multiplication);
+    }
+
+}
---
+++ Subtraction.java
@@ -0,0 +1,12 @@
+
+public class Subtraction {
+
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        int Sub = a - b;
+
+        System.out.println(Sub);
+    }
+
+}
