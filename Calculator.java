649bd756fb5c80a8601a182c5675b29c8e962c61
Yashwant0706 <yashwantjaybhaye7@gmail.com>
2026-02-04 09:11:35 +0530

    New Calculator class

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Calculator {
+    public staic void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        int operator = sc.nextInt();
+
+        switch (operator){
+
+            case 1 :
+                System.out.println(a+b);
+                break;
+
+            case 2 :
+                System.out.println(a-b);
+                break;
+            
+            case 3 :
+                System.out.println(a*b);
+                break;
+            
+            case 4 :
+    
+
+            case 5 :
+
+        }
+    }
+    
+}
